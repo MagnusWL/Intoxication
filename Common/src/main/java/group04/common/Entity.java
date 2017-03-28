cc2b13b5e88ce1c648df9e59ba7cf39de63a2c5c
burno <burno@DESKTOP-S6K2OC2>
2017-03-28 11:39:50 +0200

    Merge origin/ContainerBranch into ContainerBranch
    Conflicts:
    	Core/src/main/java/group04/core/Game.java

--- Common/src/main/java/group04/common/Entity.java
+++ Common/src/main/java/group04/common/Entity.java
@@ -17,6 +17,33 @@ public class Entity {
     private float verticalVelocity;
     private boolean hasGravity;
     private String drawable;
+     private String currentAnimation;
+    private boolean animateable = false;
+    private double currentFrame;    
+
+    public String getCurrentAnimation() {
+        return currentAnimation;
+    }
+
+    public void setCurrentAnimation(String currentAnimation) {
+        this.currentAnimation = currentAnimation;
+    }
+
+    public boolean isAnimateable() {
+        return animateable;
+    }
+
+    public void setAnimateable(boolean animateable) {
+        this.animateable = animateable;
+    }
+
+    public double getCurrentFrame() {
+        return currentFrame;
+    }
+
+    public void setCurrentFrame(double currentFrame) {
+        this.currentFrame = currentFrame;
+    }
 
     public String getDrawable() {
         return drawable;
