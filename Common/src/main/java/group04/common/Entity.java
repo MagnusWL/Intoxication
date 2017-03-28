91a90f57b67d4b89382facb0f3c7c102f273b382
burno <burno@DESKTOP-S6K2OC2>
2017-03-28 11:37:04 +0200

    Merge origin/ContainerBranch into ContainerBranch
    Conflicts:
    	Core/src/main/java/group04/core/Game.java

--- Common/src/main/java/group04/common/Entity.java
+++ Common/src/main/java/group04/common/Entity.java
@@ -11,22 +11,73 @@ public class Entity {
     private float y;
     private UUID ID = UUID.randomUUID();
     private HashMap<Class, DataContainer> dataContainer = new HashMap<>();
+    private float[] shapeX;
+    private float[] shapeY;
+    private float velocity;
+    private float verticalVelocity;
+    private boolean hasGravity;
+    private String drawable;
 
-    public void addContainer(DataContainer container)
-    {
+    public String getDrawable() {
+        return drawable;
+    }
+
+    public void setDrawable(String drawable) {
+        this.drawable = drawable;
+    }
+
+    public float getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(float velocity) {
+        this.velocity = velocity;
+    }
+
+    public float getVerticalVelocity() {
+        return verticalVelocity;
+    }
+
+    public void setVerticalVelocity(float verticalVelocity) {
+        this.verticalVelocity = verticalVelocity;
+    }
+
+    public boolean isHasGravity() {
+        return hasGravity;
+    }
+
+    public void setHasGravity(boolean hasGravity) {
+        this.hasGravity = hasGravity;
+    }
+
+    public float[] getShapeX() {
+        return shapeX;
+    }
+
+    public void setShapeX(float[] shapeX) {
+        this.shapeX = shapeX;
+    }
+
+    public float[] getShapeY() {
+        return shapeY;
+    }
+
+    public void setShapeY(float[] shapeY) {
+        this.shapeY = shapeY;
+    }
+
+    public void addContainer(DataContainer container) {
         dataContainer.put(container.getClass(), container);
     }
-  
-    public void removeContainer(Class c)
-    {
+
+    public void removeContainer(Class c) {
         dataContainer.remove(c);
     }
-  
-    public DataContainer getContainer(Class c)
-    {
+
+    public DataContainer getContainer(Class c) {
         return dataContainer.get(c);
     }
-    
+
     public EntityType getEntityType() {
         return entityType;
     }
---
+++ PlayerCommon/src/main/java/group04/playercommon/PlayerEntity.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group04.playercommon;
+
+import group04.common.Entity;
+
+/**
+ *
+ * @author burno
+ */
+public class PlayerEntity extends Entity {
+
+    private int money;
+    private float movementSpeed;
+    private float jumpSpeed;
+    private int life;
+    private int maxLife;    
+    private String weaponOwned;
+
+    public String getWeaponOwned() {
+        return weaponOwned;
+    }
+
+    public void setWeaponOwned(String weaponOwned) {
+        this.weaponOwned = weaponOwned;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getMaxLife() {
+        return maxLife;
+    }
+
+    public void setMaxLife(int maxLife) {
+        this.maxLife = maxLife;
+    }
+
+    public float getMovementSpeed() {
+        return movementSpeed;
+    }
+
+    public void setMovementSpeed(float movementSpeed) {
+        this.movementSpeed = movementSpeed;
+    }
+
+    public float getJumpSpeed() {
+        return jumpSpeed;
+    }
+
+    public void setJumpSpeed(float jumpSpeed) {
+        this.jumpSpeed = jumpSpeed;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+}
