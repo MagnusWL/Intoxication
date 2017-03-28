938742052197ec9d9b45c2875e7f4bfa2d316a2b
Mathias <Mathias@Mathias-Pc>
2017-03-28 12:33:53 +0200

    Merge origin/ContainerBranch into ContainerBranch
    Conflicts:
    	Core/src/main/java/group04/core/Game.java

---
+++ CurrencyCommon/src/main/java/group04/currencycommon/CurrencyEntity.java
@@ -0,0 +1,19 @@
+package group04.currencycommon;
+
+import group04.common.Entity;
+
+
+public class CurrencyEntity extends Entity {
+    
+    private int amount;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    
+    
+}
